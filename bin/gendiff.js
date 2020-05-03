b7248c8a6f7f90bc3888ccc6a16e3c82a509d87b
mom4uk <imanovplus@gmail.com>
2020-05-03 22:55:45 +0300

    add opportunity

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,10 +6,17 @@ program
 .version('1.0.0')
 .description('Compares two configuration files and shows a difference.')
 .arguments('<firstConfig> <secondConfig>')
-.action((firstConfig, secondConfig) => {
-    console.log('do somthing');
-  })
-
-program
+.action(genDiff = (firstPathToFile, secondPathToFile) => {
+	const path = require('path')
+	const fp = path.resolve(firstPathToFile);
+	const sp = path.resolve(secondPathToFile);
+	const fs = require('fs')
+  const data = fs.readFileSync(fp);
+  const data2 = fs.readFileSync(sp);
+  const parseDataInStr = String.fromCharCode.apply(String, data);
+  const parseData2InStr = String.fromCharCode.apply(String, data2);
+  const dataAsObject = JSON.parse(parseDataInStr);
+  const data2AsObject = JSON.parse(parseData2InStr);
+})
 .option('-f, --format [type]', 'output format')
 .parse(process.argv);
