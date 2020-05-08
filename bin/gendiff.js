76fa4a559864b51f040e33e342e36c9325cfb4f2
mom4uk <imanovplus@gmail.com>
2020-05-08 22:36:36 +0300

    add step 3

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,27 +2,39 @@
 
 const { program } = require('commander');
 
+const normalizesOutput = (coll) => {
+	const iter = (coll, acc) => {
+		const [item] = coll;
+		if (coll.length === 0) {
+			return acc + `\n${'}'}`;
+		}
+		return iter(coll.splice(1, coll.length - 1), acc + `\n ${item}`);
+	}
+	return iter(coll,'{');
+}
+
 const getDiffObj = (firstObj, secondObj) => {
 	const result = [];
 	for (const key in firstObj) {
 		for (const sKey in secondObj) {
 			if (key === sKey && firstObj[key] === secondObj[key]) {
-				result.push(`\n ${key}: ${firstObj[key]}`);
+				result.push(`${key}: ${firstObj[key]}`);
 			}
 			if (key === sKey && firstObj[key] !== secondObj[key]) {
-				result.push(`\n + ${sKey}: ${secondObj[sKey]}`);
-				result.push(`\n - ${key}: ${firstObj[key]}`);
+				result.push(`+ ${sKey}: ${secondObj[sKey]}`);
+				result.push(`- ${key}: ${firstObj[key]}`);
 			}
-			if (!secondObj.hasOwnProperty(key) && !result.includes(`\n - ${key}: ${firstObj[key]}`)) {
-				result.push(`\n - ${key}: ${firstObj[key]}`);
+			if (!secondObj.hasOwnProperty(key) && !result.includes(`- ${key}: ${firstObj[key]}`)) {
+				result.push(`- ${key}: ${firstObj[key]}`);
 			}
-			if (!firstObj.hasOwnProperty(sKey) && !result.includes(`\n + ${sKey}: ${secondObj[sKey]}`)) {
-				result.push(`\n + ${sKey}: ${secondObj[sKey]}`);
+			if (!firstObj.hasOwnProperty(sKey) && !result.includes(`+ ${sKey}: ${secondObj[sKey]}`)) {
+				result.push(`+ ${sKey}: ${secondObj[sKey]}`);
 			}
 		}
 	}
-	console.log(String(result))
+	return normalizesOutput(result);
 };
+
 
 const genDiff = (firstPathToFile, secondPathToFile) => {
 	const path = require('path');
@@ -35,7 +47,7 @@ const genDiff = (firstPathToFile, secondPathToFile) => {
   const parseData2InStr = String.fromCharCode.apply(String, data2);
   const dataAsObj = JSON.parse(parseDataInStr);
   const data2AsObj = JSON.parse(parseData2InStr);
-  getDiffObj(dataAsObj, data2AsObj);
+  console.log(getDiffObj(dataAsObj, data2AsObj));
 };
 
 program
