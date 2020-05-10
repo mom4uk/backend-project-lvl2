9066de302afae3da6f17c04134293b59b249ff94
mom4uk <imanovplus@gmail.com>
2020-05-10 22:13:51 +0300

    change readme and action

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
+##Difference calculator
+
 Utility for finding differences in configuration files.
 ##
-
-Difference calculator
 
 ![Node.js CI](https://github.com/mom4uk/backend-project-2/workflows/Node.js%20CI/badge.svg?branch=master)
 [![Maintainability](https://api.codeclimate.com/v1/badges/5ac1ebbc8f38cc9a5495/maintainability)](https://codeclimate.com/github/mom4uk/backend-project-2/maintainability)
---
+++ bin/gendiff.js
@@ -0,0 +1,61 @@
+#!/usr/bin/env node
+
+const { program } = require('commander');
+
+const normalizesOutput = (coll) => {
+	const iter = (coll, acc) => {
+		const [item] = coll;
+		if (coll.length === 0) {
+			return acc + `\n${'}'}`;
+		}
+		return iter(coll.splice(1, coll.length - 1), acc + `\n ${item}`);
+	}
+	return iter(coll,'{');
+}
+
+const getDiffObj = (firstObj, secondObj) => {
+	const result = [];
+	for (const key in firstObj) {
+		for (const sKey in secondObj) {
+			if (key === sKey && firstObj[key] === secondObj[key]) {
+				result.push(`${key}: ${firstObj[key]}`);
+			}
+			if (key === sKey && firstObj[key] !== secondObj[key]) {
+				result.push(`+ ${sKey}: ${secondObj[sKey]}`);
+				result.push(`- ${key}: ${firstObj[key]}`);
+			}
+			if (!secondObj.hasOwnProperty(key) && !result.includes(`- ${key}: ${firstObj[key]}`)) {
+				result.push(`- ${key}: ${firstObj[key]}`);
+			}
+			if (!firstObj.hasOwnProperty(sKey) && !result.includes(`+ ${sKey}: ${secondObj[sKey]}`)) {
+				result.push(`+ ${sKey}: ${secondObj[sKey]}`);
+			}
+		}
+	}
+	return normalizesOutput(result);
+};
+
+
+const genDiff = (firstPathToFile, secondPathToFile) => {
+	const path = require('path');
+	const fp = path.resolve(firstPathToFile);
+	const sp = path.resolve(secondPathToFile);
+	const fs = require('fs');
+  const data = fs.readFileSync(fp);
+  const data2 = fs.readFileSync(sp);
+  const parseDataInStr = String.fromCharCode.apply(String, data);
+  const parseData2InStr = String.fromCharCode.apply(String, data2);
+  const dataAsObj = JSON.parse(parseDataInStr);
+  const data2AsObj = JSON.parse(parseData2InStr);
+  console.log(getDiffObj(dataAsObj, data2AsObj));
+};
+
+program
+.version('1.0.0')
+.description('Compares two configuration files and shows a difference.')
+.arguments('<firstConfig> <secondConfig>')
+.action((firstPathToFile, secondPathToFile) => {
+	genDiff(firstPathToFile, secondPathToFile);
+})
+.option('-f, --format [type]', 'output format')
+.parse(process.argv);
