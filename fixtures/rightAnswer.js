8cf461ef2a085cf800801d52d2984fd933b21aa2
mom4uk <imanovplus@gmail.com>
2020-05-24 22:16:49 +0300

    fix test

--- fixtures/rightAnswer.js
+++ fixtures/rightAnswer.js
@@ -1,5 +1,6 @@
 import { fileURLToPath } from 'url';
-import { dirname, path } from 'path';
+import { dirname } from 'path';
+import path from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
