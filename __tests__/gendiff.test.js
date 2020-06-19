182abc9fb0b9359e3fd38227d49849ce40c495cf
mom4uk <imanovplus@gmail.com>
2020-06-19 19:03:26 +0300

    change tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,7 +1,18 @@
 import { test, expect } from '@jest/globals';
 import genDiff from '../gendiff';
-import { expected, dirPath1, dirPath2 } from '../fixtures/rightAnswer';
+import * as rightAnswer from '../fixtures/rightAnswer';
 
-test('output gendiff', () => {
-  expect(genDiff(dirPath1, dirPath2)).toBe(expected);
+test('output gendiff for json format', () => {
+  expect(genDiff(rightAnswer.dirPath1ForJson, rightAnswer.dirPath2ForJson))
+    .toBe(rightAnswer.expected);
+});
+
+test('output gendiff for yaml format', () => {
+  expect(genDiff(rightAnswer.dirPath1ForYaml, rightAnswer.dirPath2ForYaml))
+    .toBe(rightAnswer.expected);
+});
+
+test('output gendiff for ini format', () => {
+  expect(genDiff(rightAnswer.dirPath1ForIni, rightAnswer.dirPath2ForIni))
+    .toBe(rightAnswer.expected);
 });
--- fixtures/rightAnswer.js
+++ fixtures/rightAnswer.js
@@ -5,7 +5,13 @@ import path, { dirname } from 'path';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-export const dirPath1 = path.join(__dirname, '/before.json');
-export const dirPath2 = path.join(__dirname, '/after.json');
+export const dirPath1ForJson = path.join(__dirname, '/before.json');
+export const dirPath2ForJson = path.join(__dirname, '/after.json');
+
+export const dirPath1ForYaml = path.join(__dirname, '/before.yaml');
+export const dirPath2ForYaml = path.join(__dirname, '/after.yaml');
+
+export const dirPath1ForIni = path.join(__dirname, '/before.ini');
+export const dirPath2ForIni = path.join(__dirname, '/after.ini');
 
 export const expected = '{\n + verbose: true\n host: hexlet.io\n + timeout: 20\n - timeout: 50\n - proxy: 123.234.53.22\n - follow: false\n}';
