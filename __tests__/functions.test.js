9ff50490ae258c68f904ba4afefa556bca48d4ea
mom4uk <imanovplus@gmail.com>
2020-10-01 20:26:22 +0300

    change tests and fixtures

--- __tests__/functions.test.js
+++ __tests__/functions.test.js
@@ -11,37 +11,22 @@ const rightOutput = `{
     common: {
       + follow: false
         setting1: Value 1
-      - setting2: 200
-      - setting3: true
-      + setting3: {
-            key: value
-        }
       + setting4: blah blah
       + setting5: {
             key5: value5
         }
+      - setting2: 200
+      + setting3: {
+            key: value
+        }
+      - setting3: true
         setting6: {
-            doge: {
-              - wow: too much
-              + wow: so much
-            }
             key: value
           + ops: vops
-        }
-    }
-    group1: {
-      - baz: bas
-      + baz: bars
-        foo: bar
-      - nest: {
-            key: value
-        }
-      + nest: str
-    }
-  - group2: {
-        abc: 12345
-        deep: {
-            id: 45
+            doge: {
+              + wow: so much
+              - wow: too much
+            }
         }
     }
   + group3: {
@@ -50,6 +35,21 @@ const rightOutput = `{
             id: {
                 number: 45
             }
+        }
+    }
+    group1: {
+      + baz: bars
+      - baz: bas
+        foo: bar
+      + nest: str
+      - nest: {
+            key: value
+        }
+    }
+  - group2: {
+        abc: 12345
+        deep: {
+            id: 45
         }
     }
 }`
