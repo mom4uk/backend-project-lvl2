68690a10c25c4e1e84699ffba23800d069845743
mom4uk <imanovplus@gmail.com>
2020-10-01 21:43:12 +0300

    add dir formatters and divided diff and stylish

---
+++ formatters/stylish.js
@@ -0,0 +1,52 @@
+import isObject from 'lodash/isObject.js';
+import toPairs from 'lodash/toPairs.js';
+
+const tab = '  ';
+
+const formatKey = (key, tabulation, sign = ' ') => `${tabulation}${sign} ${key}`;
+
+const valueVerification = (val, tabulation) => {
+  if (!isObject(val)) {
+    return val;
+  }
+  const newTab = formatKey(tab, tabulation);
+  const x = toPairs(val);
+  const newX = x.flatMap(([key, value]) => {
+    if (isObject(value)) {
+      return `${formatKey(key, newTab)}: ${valueVerification(value, newTab)}`;
+    }
+    return `${formatKey(key, newTab)}: ${value}`;
+  });
+  return ['{', '\n', `${newX.join('\n')}`, '\n', `${formatKey('}', tabulation)}`].join('');
+};
+
+const stylish = (coll) => {
+  const iter = (innerColl, lvl) => {
+    const newTab = tab.repeat(lvl);
+    const result = innerColl.flatMap((obj) => {
+      const {
+        key, value, oldValue, newValue, type, children,
+      } = obj;
+      switch (type) {
+        case 'added':
+          return `${formatKey(key, newTab, '+')}: ${valueVerification(value, newTab)}\n`;
+        case 'removed':
+          return `${formatKey(key, newTab, '-')}: ${valueVerification(value, newTab)}\n`;
+        case 'changed':
+          return [`${formatKey(key, newTab, '+')}: ${valueVerification(newValue, newTab)}\n`,
+            `${formatKey(key, newTab, '-')}: ${valueVerification(oldValue, newTab)}\n`];
+        case 'unchanged':
+          return `${formatKey(key, newTab)}: ${valueVerification(value, newTab)}\n`;
+        case 'parent':
+          return `${formatKey(key, newTab)}: {\n${iter(children, lvl + 2)}${formatKey('}', newTab)}\n`;
+        default:
+          return `Wrong type ${type}`;
+      }
+    });
+    return result.join('');
+  };
+  return ['{', '\n', iter(coll, 1), '}'].join('');
+};
+
+
+export default stylish;
--- functions.js
+++ functions.js
@@ -1,56 +1,9 @@
 import isObject from 'lodash/isObject.js';
-import toPairs from 'lodash/toPairs.js';
 import { parsStrYamlToObj, parsStrIniToObj } from './parsers.js';
-
-const tab = '  ';
-
-const formatKey = (key, tabulation, sign = ' ') => `${tab}${sign} ${key}`;
-
-const valueVerification = (val, tabulation) => {
-  if (!isObject(val)) {
-    return val;
-  }
-  const newTab = formatKey(tab, tabulation);
-  const x = toPairs(val);
-  const newX = x.flatMap(([key, value]) => {
-    if (isObject(value)) {
-      return `${formatKey(key, newTab)}: ${valueVerification(value, newTab)}`;
-    }
-    return `${formatKey(key, newTab)}: ${value}`;
-  });
-  return ['{', '\n', `${newX.join('\n')}`, '\n', `${formatKey('}', tabulation)}`].join('');
-};
-
-const stylish = (coll) => {
-  const iter = (innerColl, lvl) => {
-    const newTab = tab.repeat(lvl);
-    const result = innerColl.flatMap((obj) => {
-      const {
-        key, value, oldValue, newValue, type, children,
-      } = obj;
-      switch (type) {
-        case 'added':
-          return `${formatKey(key, newTab, '+')}: ${valueVerification(value, newTab)}\n`;
-        case 'removed':
-          return `${formatKey(key, newTab, '-')}: ${valueVerification(value, newTab)}\n`;
-        case 'changed':
-          return [`${formatKey(key, newTab, '+')}: ${valueVerification(newValue, newTab)}\n`,
-            `${formatKey(key, newTab, '-')}: ${valueVerification(oldValue, newTab)}\n`];
-        case 'unchanged':
-          return `${formatKey(key, newTab)}: ${valueVerification(value, newTab)}\n`;
-        case 'parent':
-          return `${formatKey(key, newTab)}: {\n${iter(children, lvl + 2)}${formatKey('}', newTab)}\n`;
-        default:
-          return `Wrong type ${type}`;
-      }
-    });
-    return result.join('');
-  };
-  return ['{', '\n', iter(coll, 1), '}'].join('');
-};
+import stylish from './formatters/stylish.js';
 
 const isBothValuesObj = (value, value2) => {
-  if (typeof (value) === 'object' && typeof (value2) === 'object') {
+  if (isObject(value) && isObject(value2)) {
     return true;
   }
   return false;
