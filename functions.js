07d652f89cf2177f8918bab7572e1051a85d7995
mom4uk <imanovplus@gmail.com>
2020-10-14 22:07:37 +0300

    refactor getDiffObj

--- functions.js
+++ functions.js
@@ -1,48 +1,70 @@
 import { parsStrYamlToObj, parsStrIniToObj } from './parsers.js';
 import getRightFormatter from './formatters/index.js';
 import { isBothValuesObj, isIncludes } from './utils.js';
+import keys from 'lodash/keys.js';
+import flatMap from 'lodash/flatMap.js';
 
-const getDiffObj = (content1, content2, formatter) => {
-  const diff = (firstObj, secondObj) => {
-    const innerResult = [];
-    for (const key in firstObj) {
-      if (Object.prototype.hasOwnProperty.call(firstObj, key)) {
-        for (const sKey in secondObj) {
-          if (Object.prototype.hasOwnProperty.call(secondObj, sKey)) {
-            const removedObj = { key, value: firstObj[key], type: 'removed' };
-            const addedObj = { key: sKey, value: secondObj[sKey], type: 'added' };
-            const unchangedObj = { key, value: firstObj[key], type: 'unchanged' };
-            const changedObj = {
-              key, oldValue: firstObj[key], newValue: secondObj[sKey], type: 'changed',
-            };
-
-            if (key === sKey && isBothValuesObj(firstObj[key], secondObj[sKey])) {
-              innerResult.push({ key, children: diff(firstObj[key], secondObj[sKey]), type: 'parent' });
-            }
-            if (key === sKey && firstObj[key] === secondObj[sKey]) {
-              innerResult.push(unchangedObj);
-            }
-            if (key === sKey && firstObj[key] !== secondObj[sKey]
-              && !isBothValuesObj(firstObj[key], secondObj[sKey])) {
-              innerResult.push(changedObj);
-            }
-            if (!Object.prototype.hasOwnProperty.call(secondObj, key)
-              && !isIncludes(innerResult, removedObj)) {
-              innerResult.push(removedObj);
-            }
-            if (!Object.prototype.hasOwnProperty.call(firstObj, sKey)
-              && !isIncludes(innerResult, addedObj)) {
-              innerResult.push(addedObj);
-            }
-          }
-        }
-      }
+const getDiffObj = (content1, content2) => {
+  const iter = (con1, con2) => {
+  const keysContent1 = keys(con1);
+  const keysContent2 = keys(con2);
+  const result = [];
+  return keysContent1.reduce((acc, key) => {
+    if (content2.hasOwnProperty(key) && isBothValuesObj(content1[key], content2[key])) {
+      acc.push({ key, children: iter(content1[key], content2[key]), type: 'parent' });
     }
-    return innerResult;
-  };
-  const result = diff(content1, content2);
-  return getRightFormatter(result, formatter);
+    if (content2.hasOwnProperty(key) && content1[key] === content2[key]) {
+      acc.push({ key, value: content1[key], type: 'unchanged' });
+    }
+    if (!content1.hasOwnProperty(keysContent2)) { //используй has из лодаш чтобы пройти по массиву?
+      acc.push({ key, value: content1[key], type: 'removed' })
+    }
+    return acc;
+  }, result);
+  }
+  return iter(content1, content2);
 };
+// const getDiffObj = (content1, content2, formatter) => {
+//   const diff = (firstObj, secondObj) => {
+//     const innerResult = [];
+//     for (const key in firstObj) {
+//       if (Object.prototype.hasOwnProperty.call(firstObj, key)) {
+//         for (const sKey in secondObj) {
+//           if (Object.prototype.hasOwnProperty.call(secondObj, sKey)) {
+//             const removedObj = { key, value: firstObj[key], type: 'removed' };
+//             const addedObj = { key: sKey, value: secondObj[sKey], type: 'added' };
+//             const unchangedObj = { key, value: firstObj[key], type: 'unchanged' };
+//             const changedObj = {
+//               key, oldValue: firstObj[key], newValue: secondObj[sKey], type: 'changed',
+//             };
+
+//             if (key === sKey && isBothValuesObj(firstObj[key], secondObj[sKey])) {
+//               innerResult.push({ key, children: diff(firstObj[key], secondObj[sKey]), type: 'parent' });
+//             }
+//             if (key === sKey && firstObj[key] === secondObj[sKey]) {
+//               innerResult.push(unchangedObj);
+//             }
+//             if (key === sKey && firstObj[key] !== secondObj[sKey]
+//               && !isBothValuesObj(firstObj[key], secondObj[sKey])) {
+//               innerResult.push(changedObj);
+//             }
+//             if (!Object.prototype.hasOwnProperty.call(secondObj, key)
+//               && !isIncludes(innerResult, removedObj)) {
+//               innerResult.push(removedObj);
+//             }
+//             if (!Object.prototype.hasOwnProperty.call(firstObj, sKey)
+//               && !isIncludes(innerResult, addedObj)) {
+//               innerResult.push(addedObj);
+//             }
+//           }
+//         }
+//       }
+//     }
+//     return innerResult;
+//   };
+//   const result = diff(content1, content2);
+//   return getRightFormatter(result, formatter);
+// };
 
 const genDiff = (collOfReadedFiles, format, formatter) => {
   const [data1, data2] = collOfReadedFiles;
