4038ee0ae925c3dc82774d715972da9d133fd715
mom4uk <imanovplus@gmail.com>
2020-10-15 17:57:20 +0300

    finish change diff

--- functions.js
+++ functions.js
@@ -2,27 +2,36 @@ import { parsStrYamlToObj, parsStrIniToObj } from './parsers.js';
 import getRightFormatter from './formatters/index.js';
 import { isBothValuesObj, isIncludes } from './utils.js';
 import keys from 'lodash/keys.js';
-import flatMap from 'lodash/flatMap.js';
+import uniq from 'lodash/uniq.js';
 
 const getDiffObj = (content1, content2) => {
   const iter = (con1, con2) => {
   const keysContent1 = keys(con1);
   const keysContent2 = keys(con2);
-  const result = [];
-  return keysContent1.reduce((acc, key) => {
-    if (content2.hasOwnProperty(key) && isBothValuesObj(content1[key], content2[key])) {
-      acc.push({ key, children: iter(content1[key], content2[key]), type: 'parent' });
+  const sharedKeys = uniq([...keysContent1, ...keysContent2]);
+  return sharedKeys.reduce((acc, key) => {
+    if (con2.hasOwnProperty(key) && isBothValuesObj(con1[key], con2[key])) {
+      acc.push({ key, children: iter(con1[key], con2[key]), type: 'parent' });
     }
-    if (content2.hasOwnProperty(key) && content1[key] === content2[key]) {
-      acc.push({ key, value: content1[key], type: 'unchanged' });
+    if (con2.hasOwnProperty(key) && con1[key] === con2[key]) {
+      acc.push({ key, value: con1[key], type: 'unchanged' });
     }
-    if (!content1.hasOwnProperty(keysContent2)) { //используй has из лодаш чтобы пройти по массиву?
-      acc.push({ key, value: content1[key], type: 'removed' })
+    if (!con2.hasOwnProperty(key)) {
+      acc.push({ key, value: con1[key], type: 'removed'});
+    }
+    if (!con1.hasOwnProperty(key)) {
+      acc.push({ key, value: con2[key], type: 'added'});
+    }
+    if (con1.hasOwnProperty(key) && con2.hasOwnProperty(key) 
+      && con1[key] !== con2[key] && !isBothValuesObj(con1[key], con2[key])) {
+      acc.push({key , oldValue: con1[key], newValue: con2[key], type: 'changed'});
     }
     return acc;
-  }, result);
+  }, []);
   }
-  return iter(content1, content2);
+  const diff = iter(content1, content2);
+  console.log(diff[0])
+  return diff;
 };
 // const getDiffObj = (content1, content2, formatter) => {
 //   const diff = (firstObj, secondObj) => {
@@ -66,7 +75,7 @@ const getDiffObj = (content1, content2) => {
 //   return getRightFormatter(result, formatter);
 // };
 
-const genDiff = (collOfReadedFiles, format, formatter) => {
+const genDiff = (collOfReadedFiles, format, formatter) => {//поменяй название и убери функцию в utils
   const [data1, data2] = collOfReadedFiles;
   switch (format) {
     case 'yaml':
