3eb4dd2ba21ba815453e9c73b477f7a262116ce6
mom4uk <imanovplus@gmail.com>
2020-10-22 21:23:21 +0300

    refactor plain

--- formatters/plain.js
+++ formatters/plain.js
@@ -1,6 +1,4 @@
-import toPairs from 'lodash/toPairs.js';
 import isObject from 'lodash/isObject.js';
-import find from 'lodash/find.js';
 
 const formattedValue = (value) => {
   if (isObject(value)) {
@@ -18,29 +16,30 @@ const formatKey = (parentKey, key) => {
   return `${parentKey}.${key}`;
 };
 
-const processedItem = (obj, startKey = '') => {
-  const {
-    key, oldValue, newValue, value, type, children,
-  } = obj;
-  const newKey = formatKey(startKey, key);
-  switch (type) {
-    case 'added':
-      return `Property '${newKey}' was added with value: ${formattedValue(value)}`;
-    case 'removed':
-      return `Property '${newKey}' was removed`;
-    case 'changed':
-      return `Property '${newKey}' was updated. From ${formattedValue(oldValue)} to ${formattedValue(newValue)}`;
-    case 'unchanged':
-      return 'unchanged';
-    case 'parent':
-      return children.map((x) => processedItem(x, newKey)).filter((item) => item !== 'unchanged').join('\n');
-    default:
-      return `Wrong type ${type}`;
-  }
-};
-
 const plain = (diff) => {
-  const result = diff.map((item) => processedItem(item));
+  const result = diff.map((item) => {
+    const iter = (iterItem, startKey = '') => {//iterItem/innerItem/innerValue ????
+      const {
+        key, oldValue, newValue, value, type, children,
+      } = iterItem;
+      const newKey = formatKey(startKey, key);
+      switch (type) {
+        case 'added':
+          return `Property '${newKey}' was added with value: ${formattedValue(value)}`;
+        case 'removed':
+          return `Property '${newKey}' was removed`;
+        case 'changed':
+          return `Property '${newKey}' was updated. From ${formattedValue(oldValue)} to ${formattedValue(newValue)}`;
+        case 'unchanged':
+          return 'unchanged';
+        case 'parent':
+          return children.map((innerItem) => iter(innerItem, newKey)).filter((innerValue) => innerValue !== 'unchanged').join('\n');
+        default:
+          return `Wrong type ${type}`;
+      }
+    };
+    return iter(item);
+  });
   return result.sort().join('\n');
 };
 
