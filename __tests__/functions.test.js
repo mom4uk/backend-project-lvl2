aa697a05747388b2a63b1666b880be158102b75a
mom4uk <imanovplus@gmail.com>
2020-10-25 19:15:27 +0300

    refactoring tests

--- __tests__/functions.test.js
+++ __tests__/functions.test.js
@@ -71,66 +71,49 @@ Property 'group2' was removed
 Property 'group3' was added with value: [complex value]`;
 
 const rightOutputJson = '[{"key":"common","children":[{"key":"follow","value":false,"type":"added"},{"key":"setting1","value":"Value 1","type":"unchanged"},{"key":"setting2","value":200,"type":"removed"},{"key":"setting3","oldValue":true,"newValue":{"key":"value"},"type":"changed"},{"key":"setting4","value":"blah blah","type":"added"},{"key":"setting5","value":{"key5":"value5"},"type":"added"},{"key":"setting6","children":[{"key":"doge","children":[{"key":"wow","oldValue":"too much","newValue":"so much","type":"changed"}],"type":"parent"},{"key":"key","value":"value","type":"unchanged"},{"key":"ops","value":"vops","type":"added"}],"type":"parent"}],"type":"parent"},{"key":"group1","children":[{"key":"baz","oldValue":"bas","newValue":"bars","type":"changed"},{"key":"foo","value":"bar","type":"unchanged"},{"key":"nest","oldValue":{"key":"value"},"newValue":"str","type":"changed"}],"type":"parent"},{"key":"group2","value":{"abc":12345,"deep":{"id":45}},"type":"removed"},{"key":"group3","value":{"fee":100500,"deep":{"id":{"number":45}}},"type":"added"}]';
-const rightOutputJsonForIniPars = '[{"key":"common","children":[{"key":"follow","value":false,"type":"added"},{"key":"setting1","value":"Value 1","type":"unchanged"},{"key":"setting2","value":"200","type":"removed"},{"key":"setting3","oldValue":true,"newValue":{"key":"value"},"type":"changed"},{"key":"setting4","value":"blah blah","type":"added"},{"key":"setting5","value":{"key5":"value5"},"type":"added"},{"key":"setting6","children":[{"key":"doge","children":[{"key":"wow","oldValue":"too much","newValue":"so much","type":"changed"}],"type":"parent"},{"key":"key","value":"value","type":"unchanged"},{"key":"ops","value":"vops","type":"added"}],"type":"parent"}],"type":"parent"},{"key":"group1","children":[{"key":"baz","oldValue":"bas","newValue":"bars","type":"changed"},{"key":"foo","value":"bar","type":"unchanged"},{"key":"nest","oldValue":{"key":"value"},"newValue":"str","type":"changed"}],"type":"parent"},{"key":"group2","value":{"abc":"12345","deep":{"id":"45"}},"type":"removed"},{"key":"group3","value":{"fee":"100500","deep":{"id":{"number":"45"}}},"type":"added"}]';
+const rightOutputJsonForIniParser = '[{"key":"common","children":[{"key":"follow","value":false,"type":"added"},{"key":"setting1","value":"Value 1","type":"unchanged"},{"key":"setting2","value":"200","type":"removed"},{"key":"setting3","oldValue":true,"newValue":{"key":"value"},"type":"changed"},{"key":"setting4","value":"blah blah","type":"added"},{"key":"setting5","value":{"key5":"value5"},"type":"added"},{"key":"setting6","children":[{"key":"doge","children":[{"key":"wow","oldValue":"too much","newValue":"so much","type":"changed"}],"type":"parent"},{"key":"key","value":"value","type":"unchanged"},{"key":"ops","value":"vops","type":"added"}],"type":"parent"}],"type":"parent"},{"key":"group1","children":[{"key":"baz","oldValue":"bas","newValue":"bars","type":"changed"},{"key":"foo","value":"bar","type":"unchanged"},{"key":"nest","oldValue":{"key":"value"},"newValue":"str","type":"changed"}],"type":"parent"},{"key":"group2","value":{"abc":"12345","deep":{"id":"45"}},"type":"removed"},{"key":"group3","value":{"fee":"100500","deep":{"id":{"number":"45"}}},"type":"added"}]';
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
+const getContentsAndFormats = (path1, path2) => {
+  const contents = getFileContent(getFixturePath(path1), getFixturePath(path2));
+  const formats = getFileFormats(getFixturePath(path1), getFixturePath(path2));
+  return [contents, formats];
+};
+
+const getContentAndFormatJson = getContentsAndFormats('./before.json', './after.json');
+const getContentAndFormatYaml = getContentsAndFormats('./before.yaml', './after.yaml');
+const getContentAndFormatIni = getContentsAndFormats('./before.ini', './after.ini');
+
 test('output gendiff for stylish formatter', () => {
-  const collJsonData = getFileContent(getFixturePath('/before.json'), getFixturePath('/after.json'));
-  const formatFilesJson = getFileFormats(getFixturePath('/before.json'), getFixturePath('/after.json'));
-
-  const collYamlData = getFileContent(getFixturePath('/before.yaml'), getFixturePath('/after.yaml'));
-  const formatFilesYaml = getFileFormats(getFixturePath('/before.yaml'), getFixturePath('/after.yaml'));
-
-  const collIniData = getFileContent(getFixturePath('/before.ini'), getFixturePath('/after.ini'));
-  const formatFilesIni = getFileFormats(getFixturePath('/before.ini'), getFixturePath('/after.ini'));
-
-  const outputGendiffForJson = genDiff(collJsonData, formatFilesJson, formatterStylish);
+  const outputGendiffForJson = genDiff(...getContentAndFormatJson, formatterStylish);
   expect(outputGendiffForJson).toEqual(rightOutputStylish);
 
-  const outputGendiffForYaml = genDiff(collYamlData, formatFilesYaml, formatterStylish);
+  const outputGendiffForYaml = genDiff(...getContentAndFormatYaml, formatterStylish);
   expect(outputGendiffForYaml).toEqual(rightOutputStylish);
 
-  const outputGendiffForIni = genDiff(collIniData, formatFilesIni, formatterStylish);
+  const outputGendiffForIni = genDiff(...getContentAndFormatIni, formatterStylish);
   expect(outputGendiffForIni).toEqual(rightOutputStylish);
 });
 
 test('output gendiff for plain formatter', () => {
-  const collJsonData = getFileContent(getFixturePath('/before.json'), getFixturePath('/after.json'));
-  const formatFilesJson = getFileFormats(getFixturePath('/before.json'), getFixturePath('/after.json'));
-
-  const collYamlData = getFileContent(getFixturePath('/before.yaml'), getFixturePath('/after.yaml'));
-  const formatFilesYaml = getFileFormats(getFixturePath('/before.yaml'), getFixturePath('/after.yaml'));
-
-  const collIniData = getFileContent(getFixturePath('/before.ini'), getFixturePath('/after.ini'));
-  const formatFilesIni = getFileFormats(getFixturePath('/before.ini'), getFixturePath('/after.ini'));
-
-  const outputGendiffForJson = genDiff(collJsonData, formatFilesJson, formatterPlain);
+  const outputGendiffForJson = genDiff(...getContentAndFormatJson, formatterPlain);
   expect(outputGendiffForJson).toEqual(rightOutputPlain);
 
-  const outputGendiffForYaml = genDiff(collYamlData, formatFilesYaml, formatterPlain);
+  const outputGendiffForYaml = genDiff(...getContentAndFormatYaml, formatterPlain);
   expect(outputGendiffForYaml).toEqual(rightOutputPlain);
 
-  const outputGendiffForIni = genDiff(collIniData, formatFilesIni, formatterPlain);
+  const outputGendiffForIni = genDiff(...getContentAndFormatIni, formatterPlain);
   expect(outputGendiffForIni).toEqual(rightOutputPlain);
 });
 
 test('output gendiff for json formatter', () => {
-  const collJsonData = getFileContent(getFixturePath('/before.json'), getFixturePath('/after.json'));
-  const formatFilesJson = getFileFormats(getFixturePath('/before.json'), getFixturePath('/after.json'));
-
-  const collYamlData = getFileContent(getFixturePath('/before.yaml'), getFixturePath('/after.yaml'));
-  const formatFilesYaml = getFileFormats(getFixturePath('/before.yaml'), getFixturePath('/after.yaml'));
-
-  const collIniData = getFileContent(getFixturePath('/before.ini'), getFixturePath('/after.ini'));
-  const formatFilesIni = getFileFormats(getFixturePath('/before.ini'), getFixturePath('/after.ini'));
-
-  const outputGendiffForJson = genDiff(collJsonData, formatFilesJson, formatterJson);
+  const outputGendiffForJson = genDiff(...getContentAndFormatJson, formatterJson);
   expect(outputGendiffForJson).toEqual(rightOutputJson);
 
-  const outputGendiffForYaml = genDiff(collYamlData, formatFilesYaml, formatterJson);
+  const outputGendiffForYaml = genDiff(...getContentAndFormatYaml, formatterJson);
   expect(outputGendiffForYaml).toEqual(rightOutputJson);
 
-  const outputGendiffForIni = genDiff(collIniData, formatFilesIni, formatterJson);
-  expect(outputGendiffForIni).toEqual(rightOutputJsonForIniPars);
+  const outputGendiffForIni = genDiff(...getContentAndFormatIni, formatterJson);
+  expect(outputGendiffForIni).toEqual(rightOutputJsonForIniParser);
 });
